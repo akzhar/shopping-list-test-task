56977e4fcfe55092eef3f062f8cb6c6a1328da40
Akzhar Taov <akzhar@doubletapp.ai>
2021-11-25 15:56:57 +0300

    Update NotFoundPage component

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-const NotFoundPage: React.FC = () => (
-  <>404 - Not found</>
-);
+import Message from '@components/Message';
+import ActionCreator from '@store/actions';
+
+const NotFoundPage: React.FC = () => {
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(ActionCreator.set404WarningMessage());
+  });
+
+  return <Message />
+};
 
 export default NotFoundPage;
